03dee03be253ff93ee0bc88b76c5f01bbfb891df
pbishal <pbishal60@gmail.com>
2021-05-30 22:49:28 +0530

    Addresbook Table Created

--- AddressBookLinq/AddressBookDataTable.cs
+++ AddressBookLinq/AddressBookDataTable.cs
@@ -8,9 +8,30 @@ using System.Threading.Tasks;
 namespace AddressBookLinq
 {
     class AddressBookDataTable
-    {
-        // UC1 Created new address book table
+    {// UC1 Created new address book table
 
-        public static DataTable addressBookTable = new DataTable();
+        public static DataTable table = new DataTable();
+
+        /// UC2 Adds the data into table.
+        /// </summary>
+        public static void AddDataIntoTable()
+        {
+
+            //Adding columns 
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(double));
+            table.Columns.Add("Email", typeof(string));
+
+            ///Adding First Name and Last name as primary key
+            DataColumn[] primaryKeys = new DataColumn[2];
+            primaryKeys[0] = table.Columns["FirstName"];
+            primaryKeys[1] = table.Columns["LastName"];
+            table.PrimaryKey = primaryKeys;
+        }
     }
 }
